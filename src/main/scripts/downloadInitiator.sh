480532e480d2a8172fe4dd0924d91cbd4ada1c9a
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2012-05-10 11:03:56 +0200

    We now have added the downloadInitiator stub to the workflow

--- src/main/scripts/downloadInitiator.sh
+++ src/main/scripts/downloadInitiator.sh
@@ -3,4 +3,6 @@
 CONFIGFILE=$1
 INPUT=$2
 
-cat $YOUSEE_HOME/examples/download_initiator_output.json
+java  -cp $YOUSEE_HOME/downloadInitiatorLibs/ingest_initiator_media_files-*.jar:$YOUSEE_HOME/downloadInitiatorLibs/* \
+dk.statsbiblioteket.mediaplatform.ingest.ingestinitiatormediafiles.IngestInitiatorMediaFilesCLI \
+$CONFIGFILE $INPUT
