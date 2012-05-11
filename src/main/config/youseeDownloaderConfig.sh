0ef85bc2d390d1ab1e955b4f42d85e1f18a9ea3f
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2012-05-11 15:00:45 +0200

    youseeDownloader now with config

---
+++ src/main/config/youseeDownloaderConfig.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# URL to the YouSee server-location where we can download mux-files
+URL_TO_YOUSEE='http://canopus/yousee'
+
+# Path of the place to download mux-files to
+LOCALPATH='/tmp/yousee-file-downloader/'
+mkdir -p $LOCALPATH
