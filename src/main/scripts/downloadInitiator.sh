50d0093ed41524f128646ac0c38a16b6446e8d6f
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2012-05-14 15:45:43 +0200

    Download initiator changed package name

--- src/main/scripts/downloadInitiator.sh
+++ src/main/scripts/downloadInitiator.sh
@@ -7,4 +7,4 @@ INPUT=$2
 #cat $YOUSEE_HOME/examples/download_initiator_output.json
 
 java -cp $YOUSEE_HOME/components/downloadInitiatorLibs/ingest_initiator_media_files-*.jar:$YOUSEE_HOME/components/downloadInitiatorLibs/* \
-dk.statsbiblioteket.mediaplatform.ingest.ingestinitiatormediafiles.IngestInitiatorMediaFilesCLI "$CONFIGFILE" "$INPUT"
+dk.statsbiblioteket.mediaplatform.ingest.mediafilesinitiator.IngestMediaFilesInitiatorCLI "$CONFIGFILE" "$INPUT"
