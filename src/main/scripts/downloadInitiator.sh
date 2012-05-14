96b69979bba3a489b1bb4f41241e9c3683aed811
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2012-05-14 15:27:46 +0200

    Updated to the newext download initiator

--- src/main/scripts/downloadInitiator.sh
+++ src/main/scripts/downloadInitiator.sh
@@ -4,7 +4,7 @@ CONFIGFILE=$1
 INPUT=$2
 
 
-cat $YOUSEE_HOME/examples/download_initiator_output.json
+#cat $YOUSEE_HOME/examples/download_initiator_output.json
 
-#java -cp $YOUSEE_HOME/components/downloadInitiatorLibs/ingest_initiator_media_files-*.jar:$YOUSEE_HOME/components/downloadInitiatorLibs/* \
-#dk.statsbiblioteket.mediaplatform.ingest.ingestinitiatormediafiles.IngestInitiatorMediaFilesCLI "$CONFIGFILE" "$INPUT"
+java -cp $YOUSEE_HOME/components/downloadInitiatorLibs/ingest_initiator_media_files-*.jar:$YOUSEE_HOME/components/downloadInitiatorLibs/* \
+dk.statsbiblioteket.mediaplatform.ingest.ingestinitiatormediafiles.IngestInitiatorMediaFilesCLI "$CONFIGFILE" "$INPUT"
