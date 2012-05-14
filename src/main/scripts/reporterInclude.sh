0370505172a5a671ebb7a620cf99dc7f58ee5957
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2012-05-14 19:10:30 +0200

    Fixed the reporter to default be disabled Fixed the reporter to run in taverna

--- src/main/scripts/reporterInclude.sh
+++ src/main/scripts/reporterInclude.sh
@@ -7,8 +7,11 @@ function report(){
     STATE=$2
     MESSAGE=$3
     ENTITY=$4
-    echo "<state><component>$COMPONENT</component><stateName>$STATE</stateName><message>$MESSAGE</message></state>" \
-     | curl -s -i -H 'Content-Type: text/xml' -d@- $STATEMONTITORSERVER/states/$ENTITY > /dev/null
+    if [ -n $STATEMONTITORSERVER ]; then
+        echo "<state><component>$COMPONENT</component><stateName>$STATE</stateName><message>$MESSAGE</message></state>" \
+        | curl -s -i -H 'Content-Type: text/xml' -d@- $STATEMONTITORSERVER/states/$ENTITY > /dev/null
+    fi
+    return 0
 }
 
 
