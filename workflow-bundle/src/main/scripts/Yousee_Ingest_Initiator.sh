e8604b8a4ad2f3215954d6815026230569d60e95
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2012-05-24 16:08:48 +0200

    Works without the initiator

--- workflow-bundle/src/main/scripts/Yousee_Ingest_Initiator.sh
+++ workflow-bundle/src/main/scripts/Yousee_Ingest_Initiator.sh
@@ -18,11 +18,11 @@ NAME=`basename $0 .sh`
 report "$NAME" "Started" "$INPUT"
 
 
-#cat $YOUSEE_HOME/examples/download_initiator_output.json
+cat $YOUSEE_HOME/examples/download_initiator_output.json
 
 pushd $YOUSEE_CONFIG > /dev/null
-$JAVA_HOME/bin/java -cp $YOUSEE_HOME/components/downloadInitiatorLibs/ingest_initiator_impl-*.jar:$YOUSEE_HOME/components/downloadInitiatorLibs/* \
-dk.statsbiblioteket.mediaplatform.ingest.mediafilesinitiator.IngestMediaFilesInitiatorCLI "$CONFIGFILE" "$INPUT"
+#$JAVA_HOME/bin/java -cp $YOUSEE_HOME/components/downloadInitiatorLibs/ingest_initiator_impl-*.jar:$YOUSEE_HOME/components/downloadInitiatorLibs/* \
+#dk.statsbiblioteket.mediaplatform.ingest.mediafilesinitiator.IngestMediaFilesInitiatorCLI "$CONFIGFILE" "$INPUT"
 popd > /dev/null
 
 
