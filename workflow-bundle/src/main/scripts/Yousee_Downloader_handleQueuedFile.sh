57cb9c8337db690e82cde9bd98cb24c270fd8b50
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2012-05-30 16:03:34 +0200

    Fixes to the downloader handle queued

--- workflow-bundle/src/main/scripts/Yousee_Downloader_handleQueuedFile.sh
+++ workflow-bundle/src/main/scripts/Yousee_Downloader_handleQueuedFile.sh
@@ -9,15 +9,31 @@ cd `dirname ${SCRIPT_PATH}` > /dev/null
 SCRIPT_PATH=`pwd`;
 popd  > /dev/null
 
+echo "CALLED"
+echo "CALLED" >@2
+
 source $SCRIPT_PATH/env.sh
 
 ENTITY=$1
 DOWNLOADER_JSON_OUTPUT=$2
 
-LOCALNAME=`cat $DOWNLOADER_JSON_OUTPUT | grep "localName" | cut -d'"' -f4`
-YOUSEENAME=`cat $DOWNLOADER_JSON_OUTPUT | grep "youseeName" | cut -d'"' -f4`
-
 NAME=`basename $0 .sh | cut -d'_' -f-2`
-report "$NAME" "Queued" "$ENTITY"
 
-cat $DOWNLOADER_JSON_OUTPUT
+CMD="grep \"queued\": $DOWNLOADER_JSON_OUTPUT | wc -l"
+#echo "$CMD was the command"
+FOUND=`$CMD`
+#echo "$FOUND was found"
+
+if [ "$FOUND" -eq "0" ]; then \
+   cat $DOWNLOADER_JSON_OUTPUT
+   exit 0
+else
+    report "$NAME" "Queued" "$ENTITY"
+    cat $DOWNLOADER_JSON_OUTPUT
+    exit 1
+fi
+
+
+#LOCALNAME=`cat $DOWNLOADER_JSON_OUTPUT | grep "localName" | cut -d'"' -f4`
+#YOUSEENAME=`cat $DOWNLOADER_JSON_OUTPUT | grep "youseeName" | cut -d'"' -f4`
+
