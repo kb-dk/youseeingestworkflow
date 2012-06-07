11b70086d88b8667a796a51088de7fe14cf8beda
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2012-06-07 13:20:49 +0200

    install of taverna libs moved to separate script

---
+++ workflow-bundle/src/main/bin/setupTaverna.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+pushd . > /dev/null
+SCRIPT_PATH="${BASH_SOURCE[0]}";
+if ([ -h "${SCRIPT_PATH}" ]) then
+  while([ -h "${SCRIPT_PATH}" ]) do cd `dirname "$SCRIPT_PATH"`; SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`; done
+fi
+cd `dirname ${SCRIPT_PATH}` > /dev/null
+SCRIPT_PATH=`pwd`;
+popd  > /dev/null
+
+
+export COMPONENTS=$SCRIPT_PATH/../external-products
+
+
+if [ -r $SCRIPT_PATH/setenv.sh ]; then
+    source $SCRIPT_PATH/setenv.sh
+fi
+
+
+
+if [ -z "$TAVERNA_HOME" ]; then
+   echo "TAVERNA_HOME is not set. Must be set before execution. Exiting"
+   exit 1
+fi
+
+
+VERSION=`head -1 $TAVERNA_HOME/release-notes.txt | sed 's/.$//' | cut -d' ' -f4`
+LIB="$HOME/.taverna-$VERSION/lib/"
+mkdir -p $LIB
+cp -u $COMPONENTS/*.jar $LIB
--- workflow-bundle/src/main/integrationTests/setup.sh
+++ workflow-bundle/src/main/integrationTests/setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 $YOUSEE_CONFIG/test-data-canopus/cleanAll.sh
+
+YOUSEE_HOME="../"
+
+$YOUSEE_HOME/services/yousee_workflow/bin/setupTaverna.sh
