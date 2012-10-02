1d3a5c0b5e6a561b766717e910921f1fb3169f1d
ktc <ktc@statsbiblioteket.dk>
2012-10-02 11:29:34 +0200

    liberate bibliographich metadata validator from example dependency

--- yousee-ingest-workflow/src/main/scripts/yousee.bibliographic.metadata.validator.sh
+++ yousee-ingest-workflow/src/main/scripts/yousee.bibliographic.metadata.validator.sh
@@ -22,7 +22,8 @@ NAME=`basename $0 .sh`
 
 source $SCRIPT_PATH/env.sh
 
-CMD="cat $YOUSEE_HOME/examples/bibliograpthic_validator_output.json"
+#CMD="cat $YOUSEE_HOME/examples/bibliograpthic_validator_output.json"
+CMD="echo {\"valid\":true}"
 
 OUTPUT="`execute "$PWD" "$CMD" "$NAME" "$ENTITY"`"
 RETURNCODE=$?
