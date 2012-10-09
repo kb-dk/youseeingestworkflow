2d245ef84f00d58f52b9fb4b5be63d24834278e4
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2012-10-09 14:12:59 +0200

    Include the filename in the parameter list

--- yousee-ingest-workflow/src/main/scripts/crosscheck.profile.validator.sh
+++ yousee-ingest-workflow/src/main/scripts/crosscheck.profile.validator.sh
@@ -15,7 +15,7 @@ source $SCRIPT_PATH/env.sh
 
 APPDIR="$YOUSEE_COMPONENTS/${profile.validator}/"
 
-CMD="$APPDIR/bin/validateXmlWithProfile.sh $XML $CONFIGFILE $CHANNELID"
+CMD="$APPDIR/bin/validateXmlWithProfile.sh $ENTITY $XML $CONFIGFILE $CHANNELID"
 
 OUTPUT="`execute "$PWD" "$CMD" "$NAME" "$ENTITY"`"
 RETURNCODE=$?
