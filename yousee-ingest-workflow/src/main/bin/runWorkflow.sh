b1631da2eb5ad2ed32260b18423744cfd224678b
ktc <ktc@statsbiblioteket.dk>
2012-10-09 10:41:02 +0200

    changes to log rotation in accordance to review

--- yousee-ingest-workflow/src/main/bin/runWorkflow.sh
+++ yousee-ingest-workflow/src/main/bin/runWorkflow.sh
@@ -2,13 +2,15 @@
 
 SCRIPT_PATH=$(dirname $(readlink -f $0))
 
-if [ -r $SCRIPT_PATH/setenv.sh ]; then
-    source $SCRIPT_PATH/setenv.sh
+if [ -r "$SCRIPT_PATH"/setenv.sh ]; then
+    source "$SCRIPT_PATH"/setenv.sh
 fi
 
-if [ -r $SCRIPT_PATH/logRotater.sh ]; then
-    source $SCRIPT_PATH/logRotater.sh
+if [ ! -r "$SCRIPT_PATH"/logRotater.sh ]; then
+    echo "Failed to read $SCRIPT_PATH/logRotater.sh"
+    exit 1 
 fi
+source "$SCRIPT_PATH"/logRotater.sh
 
 if [ -z "$YOUSEE_HOME" ]; then
    echo "YOUSEE_HOME is not set. Must be set before execution. Exiting"
@@ -33,23 +35,23 @@ fi
 
 
 
-mkdir -p $YOUSEE_LOGS
-mkdir -p $YOUSEE_LOCKS
+mkdir -p "$YOUSEE_LOGS"
+mkdir -p "$YOUSEE_LOCKS"
 rotate_logs
 
-TAVERNA_OUT_DIR=`mktemp -d -u --tmpdir=$YOUSEE_LOGS $1-runNr-XXX`
+TAVERNA_OUT_DIR=`mktemp -d -u --tmpdir="$YOUSEE_LOGS" "$1"-runNr-XXX`
 
-TAVERNA_TEMP_DIR=`mktemp -d --tmpdir=$YOUSEE_LOGS`
+TAVERNA_TEMP_DIR=`mktemp -d --tmpdir="$YOUSEE_LOGS"`
 
-export _JAVA_OPTIONS=-Djava.io.tmpdir=$TAVERNA_TEMP_DIR
+export _JAVA_OPTIONS=-Djava.io.tmpdir="$TAVERNA_TEMP_DIR"
 
 $TAVERNA_HOME/executeworkflow.sh \
 -inmemory \
 -inputvalue Ingest_workflow_startDate "$1"  \
 "$YOUSEE_WORKFLOWS/Yousee_ingest_workflow.t2flow" \
--outputdir $TAVERNA_OUT_DIR
+-outputdir "$TAVERNA_OUT_DIR"
 
-rm -rf $TAVERNA_TEMP_DIR
+rm -rf "$TAVERNA_TEMP_DIR"
 
 exit 0
 
