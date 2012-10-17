670f24bb2edbe11b5305dd4acb885913cb7fcdf4
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2012-10-17 17:35:11 +0200

    moved the test date a few days forward, to have more channels

--- yousee-ingest-workflow/src/main/integrationTests/FirstIntegrationTest.sh
+++ yousee-ingest-workflow/src/main/integrationTests/FirstIntegrationTest.sh
@@ -9,7 +9,7 @@ echo "This tests just ensures that at least one file makes it through the workfl
 
 cd $SCRIPT_PATH/..
 
-INPUT_DATE="2012-10-12"
+INPUT_DATE="2012-10-15"
 
 if [ -d "$YOUSEE_LOGS/$INPUT_DATE" ]; then
   rm -r "$YOUSEE_LOGS/$INPUT_DATE"
